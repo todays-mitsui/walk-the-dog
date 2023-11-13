0e1669fb81ae46f60d25af06bc58711d842f569c
todays_mitsui <todays_mitsui@hatena.ne.jp>
2023-11-13 21:47:12 +0900

    .

---
+++ src/browser.rs
@@ -0,0 +1,78 @@
+use anyhow::{anyhow, Result};
+use std::future::Future;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::{closure::WasmClosureFnOnce, JsCast};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{
+    CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlImageElement, Response, Window,
+};
+
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+pub fn window() -> Result<Window> {
+    web_sys::window().ok_or(anyhow!("No Window Found"))
+}
+
+pub fn document() -> Result<Document> {
+    window()?.document().ok_or(anyhow!("No Document Found"))
+}
+
+pub fn canvas() -> Result<HtmlCanvasElement> {
+    document()?
+        .get_element_by_id("canvas")
+        .ok_or(anyhow!("No Canvas Element found with ID 'canvas'"))?
+        .dyn_into::<HtmlCanvasElement>()
+        .map_err(|element| anyhow!("Error converting {:#?} to HtmlCanvasElement", element))
+}
+
+pub fn context() -> Result<CanvasRenderingContext2d> {
+    canvas()?
+        .get_context("2d")
+        .map_err(|js_value| anyhow!("Error getting 2d context {:#?}", js_value))?
+        .ok_or(anyhow!("No 2d context found"))?
+        .dyn_into::<CanvasRenderingContext2d>()
+        .map_err(|elem| anyhow!("Error converting {:#?} to CanvasRenderingContext2d", elem))
+}
+
+pub fn spawn_local<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future);
+}
+
+pub async fn fetch_with_str(resource: &str) -> Result<JsValue> {
+    JsFuture::from(window()?.fetch_with_str(resource))
+        .await
+        .map_err(|err| anyhow!("error fetching {:#?}", err))
+}
+
+pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
+    let response_value = fetch_with_str(json_path).await?;
+    let response: Response = response_value
+        .dyn_into()
+        .map_err(|elem| anyhow!("Error converting {:#?} to Response", elem))?;
+
+    JsFuture::from(
+        response
+            .json()
+            .map_err(|err| anyhow!("Could not get JSON from response {:#?}", err))?,
+    )
+    .await
+    .map_err(|err| anyhow!("error fetching JSON {:#?}", err))
+}
+
+pub fn new_image() -> Result<HtmlImageElement> {
+    HtmlImageElement::new().map_err(|err| anyhow!("Could not create HtmlImageElement: {:#?}", err))
+}
+
+pub fn closure_onece<F, A, R>(fn_once: F) -> Closure<F::FnMut>
+where
+    F: 'static + WasmClosureFnOnce<A, R>,
+{
+    Closure::once(fn_once)
+}
---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+#[macro_use]
+mod browser;
+mod engine;
+
+// use rand::prelude::*;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::sync::Mutex;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+#[allow(unused_imports)]
+use web_sys::console;
+
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+    let context = browser::context().expect("Could not get browser context");
+
+    browser::spawn_local(async move {
+        let json = browser::fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+        let sheet: Sheet = serde_wasm_bindgen::from_value(json)
+            .expect("Could not convert rhb.json into a Sheet structure");
+
+        let image = engine::load_image("rhb.png")
+            .await
+            .expect("Could not load rhb.png");
+
+        let mut frame = -1;
+        let interval_callback = Closure::wrap(Box::new(move || {
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png", frame + 1);
+            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
+            let _ = context
+                .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                    &image,
+                    sprite.frame.x.into(),
+                    sprite.frame.y.into(),
+                    sprite.frame.w.into(),
+                    sprite.frame.h.into(),
+                    300.0,
+                    300.0,
+                    sprite.frame.w.into(),
+                    sprite.frame.h.into(),
+                );
+        }) as Box<dyn FnMut()>);
+        let _ = browser::window()
+            .unwrap()
+            .set_interval_with_callback_and_timeout_and_arguments_0(
+                interval_callback.as_ref().unchecked_ref(),
+                50,
+            );
+        interval_callback.forget();
+    });
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+    // rotated: bool,
+    // trimmed: bool,
+    // spriteSourceSize: Rect,
+}
