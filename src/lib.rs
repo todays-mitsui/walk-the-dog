3cfde59d9b993313c22195e3a515d5c10caca226
todays_mitsui <todays_mitsui@hatena.ne.jp>
2023-11-19 23:14:52 +0900

    .

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,13 @@ mod browser;
 mod engine;
 mod game;
 
-// use rand::prelude::*;
 use engine::GameLoop;
 use game::WalkTheDog;
-use serde::Deserialize;
-use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-#[allow(unused_imports)]
-use web_sys::console;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
-    let context = browser::context().expect("Could not get browser context");
 
     browser::spawn_local(async move {
         let game = WalkTheDog::new();
